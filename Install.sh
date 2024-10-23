5742c37c71f1fb825f95abce87cbb157689482bc
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-23 22:23:14 +0200

    [~] [me_MemoryPoint] is gone.

--- Install.sh
+++ Install.sh
@@ -5,7 +5,6 @@ arduino-cli \
   lib install \
     --git-url \
       https://github.com/martin-eden/Embedded-me_BaseTypes \
-      https://github.com/martin-eden/Embedded-me_MemoryPoint \
       https://github.com/martin-eden/Embedded-me_InstallStandardStreams \
       https://github.com/martin-eden/Embedded-me_UartSpeeds
 
