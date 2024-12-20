3340b174d8bd868b71cc5b5aeb12286bd66b6e3f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-12-20 16:08:37 +0200

    [~] [Example] Sync with [me_Uart].

--- examples/me_UnoAddresses/me_UnoAddresses.ino
+++ examples/me_UnoAddresses/me_UnoAddresses.ino
@@ -9,7 +9,6 @@
 
 #include <me_BaseTypes.h>
 #include <me_Uart.h>
-#include <me_UartSpeeds.h>
 #include <me_Console.h>
 
 void RunTest()
@@ -49,7 +48,7 @@ void RunTest()
 
 void setup()
 {
-  me_Uart::Init(me_UartSpeeds::Bps_115k);
+  me_Uart::Init(me_Uart::Speed_115k_Bps);
 
   Console.Print("[me_UnoAddresses] Hello there!");
   RunTest();
